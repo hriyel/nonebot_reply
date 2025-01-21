21c94f20c986148fa38d84b5f8caca09d1c5672c
hriyel <3246297450@QQ.com>
2025-01-21 23:28:35 +0800

    新功能追加

--- nonebot_reply/__init__.py
+++ nonebot_reply/__init__.py
@@ -1,8 +1,10 @@
 from nonebot.adapters import Event, Message, Bot
-from nonebot import on_message
+from nonebot import on_message,on_command
 import random
 from nonebot.plugin import PluginMetadata
 from nonebot_plugin_session import extract_session, SessionIdType
+from nonebot.rule import to_me
+from nonebot.params import CommandArg
 
 from .config import Config
 from .config import config
@@ -20,21 +22,35 @@ __plugin_meta__ = PluginMetadata(
 
 group_whitelist = config.group_whitelist  
 repeat_frequency = config.repeat_frequency
-reply = on_message(priority=1, block=False)
+reply = on_message(priority=3, block=False)
+repl = on_command("flag change",rule=to_me(),aliases={"状态调整"},priority=2,block=False)
 
 text = {}  # 存储当前消息
+flag = True
+words = ""
 
 def should_repeat(group_id): #判断是否复读  
     if random.randint(1, repeat_frequency) == 1:
         return True
 
+@repl.handle()
+async   def rel_handle(args:Message = CommandArg()):
+    global  flag    
+    if flag == True:
+        flag = False
+    else:
+        flag = True
+    status = flag
+    words = f"已将复读插件状态调整为{status}"
+    await repl.send(words)
+    
 @reply.handle()
 async def plush_handler(bot: Bot, event: Event):
     global text
     session = extract_session(bot, event)
     group_id = session.get_id(SessionIdType.GROUP).split("_")[-1]
     
-    if group_id not in group_whitelist:
+    if group_id not in group_whitelist and flag == True:
         return
     
     msg = event.get_message()
